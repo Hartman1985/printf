5174b7faf2b3f38060fd66c58a4f4800ac790836
roman-rueckert <roman.rueckert@stud.thga.de>
2022-05-18 20:23:51 +0200

    A3 format specifier in action

---
+++ format.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int i=1981;
+	float f=21.08;
+	char c='R';
+	char name[]="Roman Rückert";
+
+	printf("Integer: %i\n",i);
+	printf("Float: %.2f\n",f);
+	printf("Character: %c\n",c);
+	printf("String: %s\n",name);
+
+	return 0;
+}
+
+
